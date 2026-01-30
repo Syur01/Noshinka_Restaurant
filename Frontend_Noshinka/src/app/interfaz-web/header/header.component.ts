aaccab8a1d54ad6b686d487d08bc450b626168c0
Bryan <bp113534@gmail.com>
2026-01-30 16:59:02 +0100

    Agregacion de fotos para la web, footer completado, header completado, main terminado y form en etapa de proceso aun y tambien scroll transparente

--- Frontend_Noshinka/src/app/interfaz-web/header/header.component.ts
+++ Frontend_Noshinka/src/app/interfaz-web/header/header.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private scroller: ViewportScroller) {}
+  // Idioma por defecto
+  currentLang = 'en';
+
+  // Objeto con todos los textos del Header y secciones
+  textos: any = {
+    en: {
+      story: 'Our Story',
+      menu: 'Menu',
+      reservations: 'Reservations',
+      contact: 'Contact',
+      book: 'Book a Table',
+    },
+    es: {
+      story: 'Nuestra Historia',
+      menu: 'Carta',
+      reservations: 'Reservas',
+      contact: 'Contacto',
+      book: 'Reservar Mesa',
+    },
+  };
+  scrollTo(sectionId: string) {
+    this.scroller.scrollToAnchor(sectionId);
+  }
+  changeLanguage(lang: string) {
+    this.currentLang = lang;
+  }
+}
